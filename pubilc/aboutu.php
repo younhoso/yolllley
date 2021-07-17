30b6c94d01fe664c7afcb1d8e9ba2245ddc0a9b8
younhoso <thdbsgh3443@naver.com>
2021-07-17 21:45:28 +0900

    🎉 구축 퍼블 소스 작업 완료

--- pubilc/aboutu.php
+++ pubilc/aboutu.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="ko">
-<head>
+<head> 
     <title>aboutu</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0,maximum-scale=1.0, user-scalable=yes">
